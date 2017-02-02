467b5c8d95b2ed0d83a44c3930b859a9b30e2a00
Sukant Hajra <rrudbskr5g@snkmail.com>
2017-02-02 11:11:06 -0600

    reduce dependencies for Stack's Nix expression

--- modules/stack/default.nix
+++ modules/stack/default.nix
@@ -1,23 +1,7 @@
-{ pkgs
-, blas
-, gfortran
-, git
-, gmp
-, liblapack
-, rdkafka
-, zlib
-}:
+{ pkgs }:
 {ghc}:
 pkgs.haskell.lib.buildStackProject {
     inherit ghc;
-    name = "myEnv";
-    buildInputs = [
-        blas
-        gfortran.cc
-        git
-        gmp
-        (liblapack.override { shared = true; })
-        rdkafka
-        zlib
-    ];
+    name = "env-stack";
+    buildInputs = [ ];
 }
