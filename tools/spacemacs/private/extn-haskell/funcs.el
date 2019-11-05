b9a5d08b58768a678a40702ed19bbecc39f120d6
Sukant Hajra <rrudbskr5g@snkmail.com>
2019-11-04 21:09:26 -0600

    spacemacs: extn: functions for programmatic dir-locals specification
    Note, also cleans up functions not removed in previous commit.

--- tools/spacemacs/private/extn-haskell/funcs.el
+++ tools/spacemacs/private/extn-haskell/funcs.el
@@ -2,6 +2,24 @@
 
   ;;; public
 
+  (when (configuration-layer/layer-usedp 'extn-spacemacs)
+    (defun extn-haskell/set-dir-locals (targets &optional baselocals)
+      "Set ‘dante-target’ and other directory local variables.
+
+Setting directory-local variables with “.dir-local.el” files can be tedious.
+With this function we can set them programmatically.
+
+This function only specifies these variables only for ‘haskell-mode’. See
+‘extn-spacemacs/set-dirs-locals’ for a more general function.
+
+TARGETS is an alist associating directory names to the setting of
+‘dante-target’. BASELOCALS is an alist of other directory-local settings to
+include for all directories associated in TARGETS."
+      (let ((dirlocals
+             (cl-loop for (dir . target) in targets collect
+                      `(,dir . ((dante-target . ,target))))))
+        (extn-spacemacs/set-dirs-locals 'haskell-mode dirlocals baselocals))))
+
   (defun extn-haskell/dante-restart ()
     "‘direnv’-aware replacement for ‘dante-restart’.
 
--- tools/spacemacs/private/extn-spacemacs/funcs.el
+++ tools/spacemacs/private/extn-spacemacs/funcs.el
@@ -8,15 +8,40 @@
            (not (eq major-mode 'dired-mode)))
           (fci-mode 1)))))
 
+(defun extn-spacemacs/set-dir-locals (dir mode alist)
+  "Set directory local variables for one directory.
+
+Normally we specify directory local variables with “.dir-local.el” files. But
+with this function we can specify these variables programmatically in our Emacs
+configuration/initialization files. This function wraps
+‘dir-locals-set-class-variables’ and ‘dir-locals-set-directory-class’, which
+are a touch more verbose to use directly.
+
+DIR is a directory name as a string. MODE is a major mode symbol (use ‘nil’ for
+all major-modes). ALIST is an alist associating variables with their settings.
+
+See ‘extn-spacemacs/set-dirs-locals’ for a function to set variables for
+multiple directories at once."
+  (let ((class (extn-spacemacs//class-symbol dir mode)))
+    (dir-locals-set-class-variables class `((,mode . ,alist)))
+    (dir-locals-set-directory-class dir class)
+    (setq-default dir-locals-directory-cache (-distinct dir-locals-directory-cache))))
+
+(defun extn-spacemacs/set-dirs-locals (mode dirlocals &optional baselocals)
+  "Set directory local variables for many directories.
+
+We can specify variables for directories individually with
+‘extn-spacemacs/set-dir-locals’, but this function helps more easily specify a
+base set of variable settings for multiple directories, allowing for
+per-directory overlays.
+
+MODE is a major mode symbol (use ‘nil’ for all major-modes). DIRLOCALS is an
+alist associating directory names to the alist associating variables to their
+settings. BASELOCALS is an alist suffixed to all the alists in DIRLOCALS."
+  (cl-loop for (dir . alist) in dirlocals do
+           (extn-spacemacs/set-dir-locals dir mode (append alist baselocals))))
+
 ;;; private
-
-(defun extn-spacemacs//frame-title-format-enhanced-eval ()
-  '((:eval (extn-spacemacs//frame-title-format-enhanced))))
-
-(defun extn-spacemacs//frame-title-format-enhanced ()
-  (if (buffer-file-name)
-      (abbreviate-file-name (buffer-file-name))
-    "%b"))
 
 (defun extn-spacemacs//find-xrefs-fallingback (input kind id display-action)
   (let ((fn (intern (format "xref-backend-%s" kind)))
@@ -38,3 +63,6 @@
               (when xrefs
                 (cl-return (xref--show-xrefs xrefs display-action)))))))
       (user-error "No %s found for: %s" (symbol-name kind) input))))
+
+(defun extn-spacemacs//class-symbol (dir mode)
+  (intern (concat dir ":spacemacs-extn:" (symbol-name mode))))
