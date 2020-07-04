02751ad261d8f0ac13d11329775e67ce8f4edc1d
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-07-04 12:29:57 -0500

    make Nixpkgs import more pure

--- support/nix/default.nix
+++ support/nix/default.nix
@@ -1,7 +1,7 @@
 let
     sources = import ./sources.nix;
     nix-project-all = import sources.nix-project;
-    nixpkgs = import sources.nixpkgs {};
+    nixpkgs = import sources.nixpkgs { config = {}; overlays = []; };
 in
     { inherit nixpkgs; }
     // nix-project-all
